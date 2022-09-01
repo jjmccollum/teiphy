0c3fdcd8fe730536cbc61bb6ecddf11727d63206
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2022-09-02 09:36:26 +1000

    :cop: adding tests for CLI

--- tests/test_main.py
+++ tests/test_main.py
@@ -10,11 +10,26 @@ root_dir = Path("__file__").parent.parent
 input_example = root_dir/"example/ubs_ephesians.xml"
 
 def test_to_nexus():
-    with tempfile.NamedTemporaryFile() as output:
+    with tempfile.NamedTemporaryFile(suffix=".nexus") as output:
         result = runner.invoke(app, ["to-nexus", str(input_example), output.name])
         assert result.exit_code == 0
         assert Path(output.name).exists()
         text = Path(output.name).read_text()
         assert text.startswith("#NEXUS")
 
+
+def test_to_csv():
+    with tempfile.NamedTemporaryFile(suffix=".csv") as output:
+        result = runner.invoke(app, ["to-csv", str(input_example), output.name])
+        assert result.exit_code == 0
+        assert Path(output.name).exists()
+        text = Path(output.name).read_text()
+        assert text.startswith(",P46,P49,P92,P132,01,01C1")
+
+
+def test_to_excel():
+    with tempfile.NamedTemporaryFile(suffix=".xlsx") as output:
+        result = runner.invoke(app, ["to-excel", str(input_example), output.name])
+        assert result.exit_code == 0
+        assert Path(output.name).exists()
 
