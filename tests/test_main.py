a2844a551f77020259b5a8914e3a1dcf801dbba2
jjmccollum <jmccollum20140511@gmail.com>
2022-09-02 18:08:59 +1000

    :cop: Fixed issue with temp directories in Windows (thanks, Rob!)

--- tests/test_main.py
+++ tests/test_main.py
@@ -10,35 +10,39 @@ root_dir = Path("__file__").parent.parent
 input_example = root_dir/"example/ubs_ephesians.xml"
 
 def test_to_nexus():
-    with tempfile.NamedTemporaryFile(suffix=".nexus") as output:
-        result = runner.invoke(app, [str(input_example), output.name])
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        output = Path(tmp_dir)/"test.nexus"
+        result = runner.invoke(app, [str(input_example), str(output)])
         assert result.exit_code == 0
-        assert Path(output.name).exists()
-        text = Path(output.name).read_text()
+        assert output.exists()
+        text = output.read_text()
         assert text.startswith("#NEXUS")
 
 
 def test_to_csv():
-    with tempfile.NamedTemporaryFile(suffix=".csv") as output:
-        result = runner.invoke(app, [str(input_example), output.name])
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        output = Path(tmp_dir)/"test.csv"
+        result = runner.invoke(app, [str(input_example), str(output)])
         assert result.exit_code == 0
-        assert Path(output.name).exists()
-        text = Path(output.name).read_text()
+        assert output.exists()
+        text = output.read_text()
         assert text.startswith(",P46,P49,P92,P132,01,01C1")
 
 
 def test_to_tsv():
-    with tempfile.NamedTemporaryFile(suffix=".tsv") as output:
-        result = runner.invoke(app, [str(input_example), output.name])
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        output = Path(tmp_dir)/"test.tsv"
+        result = runner.invoke(app, [str(input_example), str(output)])
         assert result.exit_code == 0
-        assert Path(output.name).exists()
-        text = Path(output.name).read_text()
+        assert output.exists()
+        text = output.read_text()
         assert text.startswith("\tP46\tP49\tP92\tP132\t01\t01C1")
 
 
 def test_to_excel():
-    with tempfile.NamedTemporaryFile(suffix=".xlsx") as output:
-        result = runner.invoke(app, [str(input_example), output.name])
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        output = Path(tmp_dir)/"test.xlsx"
+        result = runner.invoke(app, [str(input_example), str(output)])
         assert result.exit_code == 0
-        assert Path(output.name).exists()
+        assert output.exists()
 
